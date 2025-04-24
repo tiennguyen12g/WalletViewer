9c9d40c73969c13220271118fc0598e11be88820
TNBT <tiennguyen12g@gmail.com>
2025-04-24 10:22:34 +0700

    update vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WalletViewer/',
   server:{
     host: true,
   }
