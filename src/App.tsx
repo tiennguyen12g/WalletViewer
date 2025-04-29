db62e5b9c48203601405a1b274bbd16b89ba789f
TNBT <tiennguyen12g@gmail.com>
2025-04-30 01:33:06 +0700

    fix buffer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import './App.css';
 import Home from './SolWallet/Home';
 
+// import { Buffer } from "buffer";
+
+// if (typeof window !== "undefined") {
+//   (window as any).Buffer = Buffer;
+// }
+
 function App() {
   return (
     <>
